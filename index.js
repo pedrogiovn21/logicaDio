6558421e087b16178773faac3db2e871544361dd
Pedro Giovane <157719956+pedrogiovn21@users.noreply.github.com>
2025-01-22 19:38:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let heroi = "Pedro"
+let xp= 7000
+
+if(xp <= 1000){
+    console.log("O Herói " + heroi + " está no nivel " + "Ferro" + " na sua aventura.")
+}else if(xp > 1000 && xp <= 2000){
+    console.log("O Herói " + heroi + " está no nivel " + "Bronze" + " na sua aventura.")
+}else if(xp > 2000 && xp <= 5000){
+    console.log("O Herói " + heroi + " está no nivel " + "Prata" + " na sua aventura.")
+}else if(xp > 5000 && xp <= 7000){
+    console.log("O Herói " + heroi + " está no nivel " + "Ouro" + " na sua aventura.")
+}else if(xp > 7000 && xp <= 8000){
+    console.log("O Herói " + heroi + " está no nivel " + "Platina" + " na sua aventura.")
+}else if(xp > 8000 && xp <= 9000){
+    console.log("O Herói " + heroi + " está no nivel " + "Ascendente" + " na sua aventura.")
+}else if(xp > 9000 && xp <= 10000){
+    console.log("O Herói " + heroi + " está no nivel " + "Imortal" + " na sua aventura.")
+}else if(xp >= 10001){
+    console.log("O Herói " + heroi + " está no nivel " + "Radiante" + " na sua aventura.")
+}
